14b60588560f19c4e609c09fe91b63bd663700cd
brad@smithwebtek.com <brad@smithwebtek.com>
2022-02-27 08:52:02 -0500

    Add dotenv and use it

--- movie-maker/backend/app.js
+++ movie-maker/backend/app.js
@@ -1,8 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://deploy:pointer@cluster0.y2yfx.mongodb.net/movie-maker1?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.y2yfx.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   // useCreateIndex: true,  // https://www.mongodb.com/community/forums/t/option-usecreateindex-is-not-supported/123048
   useUnifiedTopology: true,
